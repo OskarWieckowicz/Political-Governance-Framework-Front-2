cbe85151d13664aab45db8d8c1894e092d6582f5
OskarWieckowicz <oskar.wieckowicz@gmail.com>
2023-06-13 20:15:19 +0200

    payment

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,43 +1,163 @@
-import { Box, Container, Stack } from "../mui/mui";
+"use client";
 import React from "react";
-import TaxBeneficientPayment from "./TaxBeneficientPayment";
-
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "../mui/mui";
+import { RemoveIcon, AddIcon } from "../mui/mui-icons";
 interface Summary {
-  name: string;
-  value: number;
+  percentage: number;
+  destination: string;
   address: string;
+  value: number;
+  paid: number;
 }
 
 const PaymentPage = () => {
   const paymentSummaries: Summary[] = [
     {
-      name: "Education",
+      percentage: 20,
+      destination: "Education",
+      address: "0xaE4D837cAA0C53579f8a156633355Df5058B02f3",
       value: 0.3,
-      address: "0x13ikjfio131ji1o13i41i14j",
+      paid: 0.1,
     },
     {
-      name: "Health Care",
+      percentage: 20,
+      destination: "Health Care",
+      address: "0xaE4D837cAA0C53579f8a156633355Df5058B02f3",
       value: 0.5,
-      address: "0x23ikjfio1ad1o13i41i14a",
+      paid: 0.1,
     },
     {
-      name: "UE",
+      percentage: 20,
+      destination: "UE",
+      address: "0xaE4D837cAA0C53579f8a156633355Df5058B02f3",
       value: 0.2,
-      address: "0x413ikjfio1ado13i41i14x",
+      paid: 0.1,
     },
   ];
+
+  const onDecrement = () => {
+    console.log("minus");
+  };
+  const onIncrement = () => {
+    console.log("plus");
+  };
   return (
-    <Container maxWidth="sm">
-      <Stack spacing={2} marginTop="30px">
-        {paymentSummaries.map((summary) => (
-          <TaxBeneficientPayment
-            key={summary.name}
-            name={summary.name}
-            value={summary.value}
-            address={summary.address}
-          />
-        ))}
-      </Stack>
+    <Container sx={{ marginTop: "15px" }}>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Percentage</TableCell>
+              <TableCell align="center">Destination</TableCell>
+              <TableCell align="center">Address</TableCell>
+              <TableCell align="center">To be paid</TableCell>
+              <TableCell align="center">Paid</TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paymentSummaries.map((row) => (
+              <TableRow
+                key={row.destination}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center">
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      padding: "5px",
+                    }}
+                  >
+                    <IconButton onClick={onDecrement}>
+                      <RemoveIcon />
+                    </IconButton>
+                    <Typography
+                      variant="body1"
+                      display="flex"
+                      alignItems="center"
+                    >
+                      {row.percentage}
+                    </Typography>
+                    <IconButton onClick={onIncrement}>
+                      <AddIcon />
+                    </IconButton>
+                  </Box>
+                </TableCell>
+                <TableCell align="center">{row.destination}</TableCell>
+                <TableCell align="center">{row.address}</TableCell>
+                <TableCell align="center">{row.value}</TableCell>
+                <TableCell align="center">{row.paid}</TableCell>
+                <TableCell align="center">
+                  <Button variant="contained">PAY</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+            <TableRow
+              sx={{
+                "&:last-child td, &:last-child th": { border: 0 },
+                backgroundColor: "#b7f5a4",
+              }}
+            >
+              <TableCell align="center">
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    padding: "5px",
+                  }}
+                >
+                  <Typography variant="h6" display="flex" alignItems="center">
+                    100
+                  </Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center">
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    padding: "5px",
+                  }}
+                >
+                  <Typography variant="h6" display="flex" alignItems="center">
+                    1.35
+                  </Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    padding: "5px",
+                  }}
+                >
+                  <Typography variant="h6" display="flex" alignItems="center">
+                    0.4
+                  </Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
